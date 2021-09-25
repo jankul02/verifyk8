62ecba65261e50a9b58ddb9cbf3be1e92df7cc4d
janusz <janusz.kulesza@nata4d.de>
2021-09-25 15:26:36 +0200

    shell zookeeper generation

---
+++ generatingstores/zookeeper.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -eux
+
+namespace=${namespace:-"default"}
+
+if [ x${1:-""} != x ]
+then
+PASS=$1
+echo kpswd new 
+else
+PASS=$(printf dGFwb3dhMDEK | base64 --decode)
+echo PASS standard
+fi
+echo OK
+
+rm -f keystore.jks
+rm -f truststore.jks
+rm -f keystore.jks.*
+rm -f zk-*.cer
+
+for ((instanceidx=0;instanceidx<8;instanceidx++))
+do
+
+keytool -genkeypair -alias zk-${instanceidx} -keyalg RSA -keysize 2048 -dname "cn=zk-${instanceidx}" -keypass $PASS -keystore keystore.jks.${instanceidx} -storepass $PASS
+keytool -exportcert -alias zk-${instanceidx} -keystore keystore.jks.${instanceidx} -file zk-${instanceidx}.cer -rfc -storepass $PASS -keypass $PASS
+done
+
+for ((instanceidx=0;instanceidx<8;instanceidx++))
+do
+keytool -importcert -alias zk-${instanceidx} -file zk-${instanceidx}.cer -keystore truststore.jks -storepass $PASS -keypass $PASS  -noprompt
+done
+
+printf "" > keystores.base64.secret.yaml
+
+for ((instanceidx=0;instanceidx<8;instanceidx++))
+do
+PASSBASE64=$(printf $PASS  | base64 -w0)
+
+cat << EOFKEYSTORE >>keystores.base64.secret.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: keystore-zk-${instanceidx}
+  namespace: ${namespace}
+data:
+  keystore.jks: "$(cat keystore.jks.${instanceidx} | base64 -w0 )"
+  truststore.jks: "$(cat truststore.jks | base64 -w0)"
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: keystorepasswd-zk-${instanceidx}
+  namespace: ${namespace}
+data:
+  keystorepassword: ${PASSBASE64} 
+  truststorepassword: ${PASSBASE64}
+---
+EOFKEYSTORE
+
+
+done
+
